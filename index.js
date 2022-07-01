73987b628e4b534e016b46371fe7b7bf11fffb71
Shiva Adigopula <vskrao.adigopula@gmail.com>
2022-07-01 13:17:11 +0400

    added index.ts file and compiled this js file using 'tsc index.ts' command

--- index.js
+++ index.js
@@ -1,16 +1,9 @@
-
 var button = document.querySelector("button");
-var input1 = document.getElementById("input1"); // 1
-var input2 = document.getElementById("input2"); // 1
-
-function add(num1, num2){
-    return num1+num2; 
+var input1 = document.getElementById("input1");
+var input2 = document.getElementById("input2");
+function add(num1, num2) {
+    return num1 + num2;
 }
-
-button.addEventListener('click', function() {
+button.addEventListener('click', function () {
     console.log(add(+input1.value, +input2.value));
-    //input1.value is string
-    //input1.value is string
-
-    // to convert string to number , prefix it with +
 });
