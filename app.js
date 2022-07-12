ae96f126438692069456e3ff534e1534ada77597
Shiva Adigopula <vskrao.adigopula@gmail.com>
2022-07-12 10:23:39 +0400

    Argument of type string is not assignable to parameter of type number.

---
+++ app.js
@@ -0,0 +1,6 @@
+function add(num1, num2) {
+    return num1 + num2;
+}
+var number1 = '5';
+var number2 = 10;
+console.log(add(number1, number2));
---
+++ app.ts
@@ -0,0 +1,7 @@
+function add(num1:number, num2:number) { 
+    return num1 + num2;
+}
+
+const number1 = '5';
+const number2 = 10;
+console.log(add(number1, number2));
