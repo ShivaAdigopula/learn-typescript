ffa4494ac525475e25387de08f5737e0c8c5247d
Shiva Adigopula <vskrao.adigopula@gmail.com>
2022-07-12 12:31:41 +0400

    literal type example

--- app.ts
+++ app.ts
@@ -5,14 +5,16 @@ interface Person  {
     name: string,
     age: number | string,
     hobbies: string[],
-    role: Role // tuple  => Fixed length array
+    role: Role // tuple  => Fixed length array,
+    county: 'India' | 'Rest Of The World'
 }
 
 const person:Person = {
     name: 'Shiva',
     age: '29',
     hobbies: ['Listening Music', 'Watching Movies'],
-    role: Role.ADMIN
+    role: Role.ADMIN,
+    county: 'India'
 };
 
 console.log(person.name);
