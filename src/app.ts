5a17cae93b0810d3062664afdea2d0c86546b24a
Shiva Adigopula <vskrao.adigopula@gmail.com>
2022-07-14 17:00:26 +0400

    function overloading

--- src/app.ts
+++ src/app.ts
@@ -20,6 +20,21 @@ type Numeric = number | boolean;
 
 type Universal = Combinable | Numeric;
 
+// function oveloading
+function add(a: number, b: number): number;
+function add(a: string, b: string): string;
+function add(a: Combinable, b: Combinable) { 
+    if (typeof a === 'string' || typeof b === 'string') { 
+        return a.toString() + b.toString();
+    }
+    return a + b;
+}
+
+const sum = add(2, 3);
+console.log(sum);
+
+const conc = add('shiva', ' adigopula');
+console.log(conc.split(' ')); // without overloading methods it gives error
 
 interface Bird { 
     type: 'bird';
